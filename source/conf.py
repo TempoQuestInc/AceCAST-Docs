ea76466b4ddd343dfe4e8dac43fedb056f3dc1ab
sael9740 <sael9740@colorado.edu>
2021-10-08 20:35:32 -0600

    added import statement

--- source/conf.py
+++ source/conf.py
@@ -27,6 +27,7 @@ author = 'Jan Ising'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+import sphinx_copybutton
 extensions = [
     'sphinx_copybutton',
 ]
