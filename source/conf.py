d8316ac2ae0a789ec017b2abca5dcffc65ef790d
sael9740 <sael9740@colorado.edu>
2021-10-12 11:02:16 -0600

    forgot to comment copybutton line

--- source/conf.py
+++ source/conf.py
@@ -28,7 +28,7 @@ author = 'Jan Ising'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_copybutton',
+    # 'sphinx_copybutton',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
